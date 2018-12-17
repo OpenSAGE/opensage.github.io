b49bc10627786012ad37386a92575694ec4b86f2
Paavo Huhtala <paavo.huhtala@gmail.com>
2018-12-17 03:21:29 +0200

    Generate thumbnails for all images regardless of size

--- scripts/generate-thumbnails.js
+++ scripts/generate-thumbnails.js
@@ -3,8 +3,6 @@ const glob = require("glob")
 const fs = require("fs")
 const Jimp = require("jimp")
 
-// Resize images larger than this (in bytes)
-const resizeThreshold = 1024 * 500
 // Thumbnails must fit within a rectangle sized width*height
 const maxThumbnailSize = [1000, 562]
 
@@ -18,34 +16,25 @@ const tasks = []
 
 for (const relativePath of files) {
   const fullPath = path.resolve(assetsRoot, relativePath)
-  const stat = fs.statSync(fullPath)
 
-  if (stat.size <= resizeThreshold) {
-    console.log(
-      `Skipping ${relativePath} (size ${Math.ceil(stat.size / 1024)} kb)`
-    )
-    continue
-  }
+  tasks.push(
+    Jimp.read(fullPath).then(image => {
+      const pathParts = path.parse(fullPath)
 
-  tasks.push(Jimp.read(fullPath))
+      const thumbPath = path.format({
+        dir: pathParts.dir,
+        name: pathParts.name + "-thumb",
+        ext: ".jpg"
+      })
 
-  const task = Jimp.read(fullPath).then(image => {
-    const pathParts = path.parse(fullPath)
+      console.log(`Processing ${relativePath}...`)
 
-    const thumbPath = path.format({
-      dir: pathParts.dir,
-      name: pathParts.name + "-thumb",
-      ext: ".jpg"
+      return image
+        .scaleToFit(maxThumbnailSize[0], maxThumbnailSize[1])
+        .quality(95)
+        .writeAsync(thumbPath)
     })
-
-    console.log(`Processing ${relativePath}...`)
-
-    return image
-      .scaleToFit(maxThumbnailSize[0], maxThumbnailSize[1])
-      .quality(95)
-      .writeAsync(thumbPath)
-  })
-  tasks.push(task)
+  )
 }
 
 Promise.all(tasks).then(() => {

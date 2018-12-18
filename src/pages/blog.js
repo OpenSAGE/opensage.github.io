4f0298dc46695617804b4a8977d962451dde45d3
paavohuhtala <paavo.huhtala@gmail.com>
2018-12-18 16:56:44 +0200

    Group blog posts by year, tweak margins

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,37 @@
 import React from "react"
 import { withRouteData, Head } from "react-static"
 import { Link } from "@reach/router"
+import moment from "moment"
 
-export default withRouteData(props => (
-  <>
-    <Head>
-      <title>Blog - OpenSAGE</title>
-    </Head>
-    <h1>The OpenSAGE blog</h1>
-    All Posts:
-    <ul>
-      {props.posts.map(post => (
-        <li key={post.id}>
-          <Link to={`/blog/post/${post.id}/`}>
-            {post.title} ({post.publishedDate})
-          </Link>
-        </li>
+import { groupBy, uniq, orderBy } from "lodash"
+
+const Post = ({ post }) => (
+  <li key={post.id}>
+    <Link to={`/blog/post/${post.id}/`}>
+      {post.title} ({post.publishedDate})
+    </Link>
+  </li>
+)
+
+export default withRouteData(({ posts }) => {
+  const postsByYear = groupBy(posts, post => moment(post.publishedDate).year())
+  const years = orderBy(uniq(Object.keys(postsByYear))).reverse()
+  return (
+    <>
+      <Head>
+        <title>Blog - OpenSAGE</title>
+      </Head>
+      <h1>The OpenSAGE blog</h1>
+      {years.map(year => (
+        <React.Fragment key={year}>
+          <h2 key={year}>{year}</h2>
+          <ul>
+            {postsByYear[year].map(post => (
+              <Post key={post.id} post={post} />
+            ))}
+          </ul>
+        </React.Fragment>
       ))}
-    </ul>
-  </>
-))
+    </>
+  )
+})

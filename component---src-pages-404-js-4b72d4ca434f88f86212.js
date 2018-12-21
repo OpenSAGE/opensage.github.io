92720e28cc3723334230b6117b40ab99df34fccf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-21 18:29:18 +0000

    Deploy OpenSAGE/opensage.github.io to github.com/OpenSAGE/opensage.github.io.git:master

---
+++ component---src-pages-404-js-4b72d4ca434f88f86212.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{141:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"404 - Oh no's! We couldn't find that page :("))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4b72d4ca434f88f86212.js.map

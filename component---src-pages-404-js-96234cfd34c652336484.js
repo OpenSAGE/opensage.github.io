b143f5e1e392d628f13a2f7eb7bb6abb30fccd01
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-23 07:36:25 +0000

    Deploy OpenSAGE/opensage.github.io to github.com/OpenSAGE/opensage.github.io.git:master

---
+++ component---src-pages-404-js-96234cfd34c652336484.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),l=t.n(a);e.default=function(){return l.a.createElement("div",null,l.a.createElement("h1",null,"404 - Oh no's! We couldn't find that page :("))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-96234cfd34c652336484.js.map
